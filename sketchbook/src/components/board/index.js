aa62df90f64b42cb559bb70ef120eb7f70f63780
Devang2304 <devangvartak2304@gmail.com>
2024-06-02 22:48:56 +0530

    added draw feature

--- sketchbook/src/components/board/index.js
+++ sketchbook/src/components/board/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import { useRef,useEffect } from 'react';
 import { useSelector,useDispatch } from 'react-redux';
 
 const  Board = () => {
     const canvasRef=useRef(null);
+    const shouldDraw=useRef(false);
     const activeMenuItem=useSelector((state)=>state.menu.activeMenuItem);
     const {color,size}=useSelector((state)=>state.toolbox[activeMenuItem]);
 
@@ -12,9 +13,47 @@ const  Board = () => {
       const canvas = canvasRef.current;
       const context=canvas.getContext('2d');
 
+      const changeConfig=()=>{
+        context.strokeStyle=color;
+        context.lineWidth=size;
+      }
+      changeConfig();
+    },[color,size])
+    //mount
+    useLayoutEffect(() => {
+      if(!canvasRef.current) return;
+      const canvas = canvasRef.current;
+      const context=canvas.getContext('2d');
+
       // mounting 
       canvas.width=window.innerWidth;
       canvas.height=window.innerHeight;
+
+
+      const handleMouseDown=(e)=>{
+        shouldDraw.current=true;
+        context.beginPath(); // initialise
+        context.moveTo(e.clientX, e.clientY);
+      }
+      const handleMouseUp=(e)=>{
+        shouldDraw.current=false;
+        // context.closePath();
+      }
+      const handleMouseMove=(e)=>{
+        if(!shouldDraw.current) return;
+        context.lineTo(e.clientX, e.clientY); //draw
+        context.stroke();
+      }
+
+      canvas.addEventListener('mousedown',handleMouseDown);
+      canvas.addEventListener('mouseup', handleMouseUp);
+      canvas.addEventListener('mousemove', handleMouseMove);
+      return ()=>{
+        canvas.removeEventListener('mousedown', handleMouseDown);
+        canvas.removeEventListener('mouseup', handleMouseUp);
+        canvas.removeEventListener('mousemove', handleMouseMove);
+      }
+      //unmount
     },[]);
 
     console.log(color,size);
