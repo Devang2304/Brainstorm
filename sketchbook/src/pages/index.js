b2a136056800508d819dffc7ccf0ef423dea9292
Devang2304 <devangvartak2304@gmail.com>
2024-06-02 19:29:36 +0530

    added redux toolkit and made communication between menu and toolbox

--- sketchbook/src/pages/index.js
+++ sketchbook/src/pages/index.js
@@ -1,11 +1,13 @@
 import Menu from "@/components/menu";
 import Toolbox from "@/components/toolbox";
+import Board from "@/components/board";
 
 export default function Home() {
   return (
     <main>
       <Menu />
       <Toolbox />
+      <Board />
     </main>
   );
 }
