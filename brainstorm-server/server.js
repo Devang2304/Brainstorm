a0e57d649c451a42ca59caa892ad9fb80eba29b9
Devang2304 <devangvartak2304@gmail.com>
2024-06-03 19:48:37 +0530

    whitelisted client

--- brainstorm-server/server.js
+++ brainstorm-server/server.js
@@ -5,10 +5,12 @@ const {Server} = require('socket.io');
 const cors=require('cors');
 
 const app=express();
-app.use(cors({origin:"http://localhost:3000"}));
+const isDev = app.settings.env === 'development';
+const URL= isDev ? 'http://localhost:3000': 'https://brainstorm-ten.vercel.app'
+app.use(cors({origin:URL}));
 const httpServer=createServer(app);
 const io = new Server(httpServer, {
-    cors:"http://localhost:3000"
+    cors:URL
 
 })
 
