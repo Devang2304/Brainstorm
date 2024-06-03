80e7ab6c7ac916a010ee9bcca6501af76c805e8f
Devang2304 <devangvartak2304@gmail.com>
2024-06-03 19:18:16 +0530

    added realtime sync backend

--- brainstorm-server/server.js
+++ brainstorm-server/server.js
@@ -13,7 +13,18 @@ const io = new Server(httpServer, {
 })
 
 io.on("connection",(socket)=>{
-    console.log("Ohh fuck server connected!!");
+    console.log("server connected!!");
+
+    socket.on('beginPath',(arg)=>{
+        socket.broadcast.emit('beginPath',arg);
+    });
+    socket.on('drawLine',(arg)=>{
+        socket.broadcast.emit('drawLine',arg);
+    });
+
+    socket.on('changeConfig',(arg)=>{
+        socket.broadcast.emit('changeConfig', arg);    
+    })
 });
 
 httpServer.listen(4000);
